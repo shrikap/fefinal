37f8067ecacd3a1ed9dd4c8012fd2570c78d6aae
shrikap <shrikapunuganti@gmail.com>
2023-07-19 10:57:59 -0700

    Initial Commit

---
+++ PLSWORK/PLSWORKApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLSWORKApp.swift
+//  PLSWORK
+//
+//  Created by Scholar on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct PLSWORKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
